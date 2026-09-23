6767144aa29ed8975b335da4343895693aff04d0
Brian McFee <brian.mcfee@nyu.edu>
2026-09-23 09:49:27 -0400

    linting action

--- bopp/registries/__init__.py
+++ bopp/registries/__init__.py
@@ -1,12 +1,14 @@
-from typing import Any, Dict
+from typing import Any
 
-def get_registry(version: str) -> Dict[str, Any]:
+
+def get_registry(version: str) -> dict[str, Any]:
     """
     Resolve the target registry and Annotation class based on the bopp_version.
     """
     if version in ("1.0.0", "v1", "1"):
-        from . import v1
         from bopp.models.v1.annotation import Annotation
+
+        from . import v1
         
         return {
             "PAYLOAD_TYPE_REGISTRY": v1.PAYLOAD_TYPE_REGISTRY,
