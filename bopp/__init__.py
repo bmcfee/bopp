772385a356cdae31ab9bdf6692cfb01844677407
Brian McFee <brian.mcfee@nyu.edu>
2026-09-23 12:17:59 -0400

    exporting api

--- bopp/__init__.py
+++ bopp/__init__.py
@@ -2,4 +2,4 @@ from . import io as io
 from . import models as models
 from . import registries as registries
 from . import util as util
-from .core import create as create
+from .core import create as create, validate as validate
