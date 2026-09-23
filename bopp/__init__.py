daa33f78fe5be029d5004b9da847ded704c40d55
Brian McFee <brian.mcfee@nyu.edu>
2026-09-23 12:24:22 -0400

    ruffing

--- bopp/__init__.py
+++ bopp/__init__.py
@@ -2,4 +2,5 @@ from . import io as io
 from . import models as models
 from . import registries as registries
 from . import util as util
-from .core import create as create, validate as validate
+from .core import create as create
+from .core import validate as validate
